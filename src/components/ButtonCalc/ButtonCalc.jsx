18cfb998d5bff4e7aae01068a1864ecb1208aa6c
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-11-07 00:49:14 -0700

    prevent consecutive operators

--- src/components/ButtonCalc/ButtonCalc.jsx
+++ src/components/ButtonCalc/ButtonCalc.jsx
@@ -11,7 +11,29 @@ const ButtonCalc = ({ btn, setDisplayValue, displayValue }) => {
         calculateNum(displayValue);
         return;
       case '^':
-        setDisplayValue(displayValue + '**');
+        consecutiveOperatorError(displayValue)
+          ? setDisplayValue(displayValue)
+          : setDisplayValue(displayValue + btn.value);
+        return;
+      case '+':
+        consecutiveOperatorError(displayValue)
+          ? setDisplayValue(displayValue)
+          : setDisplayValue(displayValue + btn.value);
+        return;
+      case '-':
+        consecutiveOperatorError(displayValue)
+          ? setDisplayValue(displayValue)
+          : setDisplayValue(displayValue + btn.value);
+        return;
+      case '*':
+        consecutiveOperatorError(displayValue)
+          ? setDisplayValue(displayValue)
+          : setDisplayValue(displayValue + btn.value);
+        return;
+      case '/':
+        consecutiveOperatorError(displayValue)
+          ? setDisplayValue(displayValue)
+          : setDisplayValue(displayValue + btn.value);
         return;
       default:
         setDisplayValue(displayValue + btn.value);
@@ -20,14 +42,33 @@ const ButtonCalc = ({ btn, setDisplayValue, displayValue }) => {
   };
 
   const calculateNum = (expression) => {
-    const evaluatedNum = Function(`'use strict'; return (${expression})`)();
+    const updatedExpression = expression.replace('^', '**');
+    const evaluatedNum = Function(
+      `'use strict'; return (${updatedExpression})`
+    )();
     setDisplayValue(evaluatedNum);
+  };
+
+  const consecutiveOperatorError = (expression) => {
+    const lastChar = expression[expression.length - 1];
+    if (
+      lastChar === '/' ||
+      lastChar === '*' ||
+      lastChar === '+' ||
+      lastChar === '-' ||
+      lastChar === '^'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return (
     <Button
       onClick={handleBtnClick}
       data-testid={btn.value}
+      // disableTouchRipple
       //   variant='outlined'
       sx={{
         minHeight: 95,
@@ -38,6 +79,7 @@ const ButtonCalc = ({ btn, setDisplayValue, displayValue }) => {
         border: btn.value === '=' ? '2px solid black' : '2px solid #6CC3F7',
         background: btn.value === '=' ? '#6CC3F7' : 'white',
       }}
+      className='ripple'
     >
       {btn.value}
     </Button>
